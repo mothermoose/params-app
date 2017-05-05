4f92cf7feb2a356b963ea60fbab7e0eca5ef423f
mothermoose <melanie.ilseman@gmail.com>
2017-05-04 21:25:37 -0500

    forms addition

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -7,4 +7,11 @@ class ParamsExamplesController < ApplicationController
   def url_segment
     @message = params["this_is_a_key"]
   end
+
+  def form_show
+  end  
+
+  def form_send
+    @message = params["form_message"]
+  end 
 end
